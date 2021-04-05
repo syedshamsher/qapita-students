b18e00d4daf51fc005d1ba3ee49677912c57ffa6
syedshamsher <shamsher78619@gmail.com>
2021-04-05 11:55:21 +0530

    ROUTER-DOM: Added React-Router-Dom

---
+++ frontend/src/routes/Routes.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Switch, Route } from 'react-router-dom';
+
+export const Routes = () => {
+    return (
+        <div>
+            <Switch>
+                <Route></Route>
+            </Switch>
+        </div>
+    )
+}
